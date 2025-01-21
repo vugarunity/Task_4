fc532acdaec19aafddbae04ab91a1154cdb3fac5
vugarunity <159554692+vugarunity@users.noreply.github.com>
2025-01-21 12:56:37 +0400

    Update UITest.java

--- src/test/java/org/example/UITest.java
+++ src/test/java/org/example/UITest.java
@@ -11,40 +11,26 @@ public class UITest extends BaseTest {
 
     @Test
     void testGBNotEmailLogin() {
-        // Используем PageFactory для инициализации LoginPage
         LoginPage loginPage = new LoginPage(driver, new WebDriverWait(driver, Duration.ofSeconds(10)));
         loginPage.openLoginPage();
 
-
-        // Вводим данные
         loginPage.EmailInField("");
         loginPage.PasswordInField("");
         loginPage.clickLoginButton();
 
-        // Проверяем, что ошибка для email отображается с ожидаемым текстом
-//        String expectedErrorMessage = "Обязательное поле.";
-//        String actualErrorMessage = loginPage.getEmailError();
-
         Assertions.assertTrue(loginPage.getEmailError().contains("Обязательное поле"), " ");
 
         Assertions.assertTrue(loginPage.isElementDisplayed(loginPage.emailError), "Неверный email или пароль.");
-
-//        Assertions.assertEquals(expectedErrorMessage, actualErrorMessage, " ");
-
-
     }
 
     @Test
     void testGBWithoutPassword() {
-        // Используем PageFactory для инициализации LoginPage
         LoginPage loginPage = new LoginPage(driver, new WebDriverWait(driver, Duration.ofSeconds(10)));
         loginPage.openLoginPage();
 
-        // Вводим email и оставляем пароль пустым
         loginPage.PasswordInField(" ");
         loginPage.clickLoginButton();
 
-        // Проверяем, отображается ли ошибка для пароля
         Assertions.assertTrue(loginPage.getPasswordError().contains("Обязательное поле"), " ");
     }
 }
