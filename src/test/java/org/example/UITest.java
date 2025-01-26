4fe0dcf56f9cc80a6ca6bc22cd2faaa5f55552e5
vugarunity <159554692+vugarunity@users.noreply.github.com>
2025-01-26 20:41:13 +0400

    Update UITest.java

--- src/test/java/org/example/UITest.java
+++ src/test/java/org/example/UITest.java
@@ -10,17 +10,27 @@ import java.time.Duration;
 public class UITest extends BaseTest {
 
     @Test
-    void testGBNotEmailLogin() {
+    void testGBInvalidLoginCredentials_shouldShowGeneralError() {
         LoginPage loginPage = new LoginPage(driver, new WebDriverWait(driver, Duration.ofSeconds(10)));
         loginPage.openLoginPage();
 
-        loginPage.EmailInField("");
-        loginPage.PasswordInField("");
+        loginPage.EmailInField("login");
+        loginPage.PasswordInField("password");
         loginPage.clickLoginButton();
 
-        Assertions.assertTrue(loginPage.getEmailError().contains("Обязательное поле"), " ");
-
         Assertions.assertTrue(loginPage.isElementDisplayed(loginPage.emailError), "Неверный email или пароль.");
+    }
+
+    @Test
+    void testGBEmptyEmailAndPassword_shouldShowRequiredFieldErrors() {
+        LoginPage loginPage = new LoginPage(driver, new WebDriverWait(driver, Duration.ofSeconds(10)));
+        loginPage.openLoginPage();
+
+        loginPage.EmailInField(" ");
+        loginPage.PasswordInField(" ");
+        loginPage.clickLoginButton();
+
+        Assertions.assertTrue(loginPage.getEmailError().contains("Обязательное поле"), "Обязательное поле не отображается");
     }
 
     @Test
@@ -28,9 +38,11 @@ public class UITest extends BaseTest {
         LoginPage loginPage = new LoginPage(driver, new WebDriverWait(driver, Duration.ofSeconds(10)));
         loginPage.openLoginPage();
 
+        loginPage.EmailInField("test@example.com");
         loginPage.PasswordInField(" ");
         loginPage.clickLoginButton();
-
-        Assertions.assertTrue(loginPage.getPasswordError().contains("Обязательное поле"), " ");
+        
+        Assertions.assertTrue(loginPage.getPasswordError().contains("Обязательное поле"), "Обязательное поле не отображается");
     }
 }
+
